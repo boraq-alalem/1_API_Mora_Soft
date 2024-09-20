3ca1e3b95ab94933c6e271c72a366b4acc15fdb9
boraq-alalem <alalemhammam@gmail.com>
2024-09-20 10:08:00 +0200

    Create FactoryPost, to make fake posts for test

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        return "Hello";
+        // return "Hello";
+        $posts = Post::get();
+        return $posts;
     }
 }
